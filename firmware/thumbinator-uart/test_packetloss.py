472900b8901a8bc3d52a095312e97b891b6f1c61
Konrad Beckmann <konrad.beckmann@gmail.com>
2016-10-18 00:29:45 +0200

    Initial commit woo!

---
+++ firmware/thumbinator-uart/test_packetloss.py
@@ -0,0 +1,38 @@
+#!/bin/env python2
+import time
+
+f = open("/dev/ttyACM0", "rb")
+
+# first line will be partial
+l = f.readline()
+
+def get_index(f):
+    line = f.readline()
+    data = line.split(" ")
+    index = int(data[0], 16)
+    return index, line
+
+miss = 0
+tot = 0
+num_bytes = 0
+t0 = time.time()
+
+x, data = get_index(f)
+num_bytes += len(data)
+
+try:
+    while True:
+        newx, data = get_index(f)
+        num_bytes += len(data)
+        tot += 1
+        if newx - x != 1:
+            miss += 1
+            print "missed packet. %d/%d (%d => %d)" % (miss, tot, x, newx)
+        x = newx
+except KeyboardInterrupt:
+    t1 = time.time()
+    tdiff = t1 - t0
+    kilobytes_per_second = (num_bytes / 1024.0) / tdiff
+    print ""
+    print "Missed %d packets out of %d total" % (miss, tot)
+    print "Collected %d bytes during %.2f seconds. %.2f kB/s" % (num_bytes, tdiff, kilobytes_per_second)
