8be0b9d5ae0eab76fcc3bf634693074c42fee869
Simon Vikstrom <simon@devsn.se>
2017-12-15 22:41:21 +0100

    Nastyness

--- firmware/thumbinator-ws2812b/Src/effect.c
+++ firmware/thumbinator-ws2812b/Src/effect.c
@@ -11,9 +11,9 @@
 
 static int current_effect;
 static uint8_t g_pixels[2][NUM_LEDS * 3];
-typedef void (*effect_render_cb)(uint32_t t, uint8_t * p_pixels, uint32_t num_bytes);
+typedef void (*effect_render_cb)(uint32_t index, uint32_t t, uint8_t * p_pixels, uint32_t num_bytes);
 
-void render_colors(uint32_t t, uint8_t * p_pixels, uint32_t num_bytes)
+void render_colors(uint32_t index, uint32_t t, uint8_t * p_pixels, uint32_t num_bytes)
 {
     uint8_t h, s, v;
 
@@ -30,7 +30,7 @@ void render_colors(uint32_t t, uint8_t * p_pixels, uint32_t num_bytes)
     }
 }
 
-void render_constant(uint32_t t, uint8_t * p_pixels, uint32_t num_bytes)
+void render_constant(uint32_t index, uint32_t t, uint8_t * p_pixels, uint32_t num_bytes)
 {
     uint8_t h, s, v;
     uint8_t r, g, b;
@@ -47,9 +47,57 @@ void render_constant(uint32_t t, uint8_t * p_pixels, uint32_t num_bytes)
     }
 }
 
+void render_car(uint32_t index, uint32_t t, uint8_t * p_pixels, uint32_t num_bytes)
+{
+    uint32_t middle = (num_bytes / 2)+3;
+    for (int i = 0; i < num_bytes; i += 3) {
+
+        if(i > middle - 6*3 && i < middle + 6*3){
+            p_pixels[i]     = 100;
+            p_pixels[i + 1] = 100;
+            p_pixels[i + 2] = 100;
+            continue;
+        }
+
+
+        if(i < 10*3 || i > num_bytes - 10*3 ){
+            p_pixels[i]     = 0;
+            p_pixels[i + 1] = 100;
+            p_pixels[i + 2] = 0;
+            continue;
+        }
+
+
+        p_pixels[i]     = 0;
+        p_pixels[i + 1] = 0;
+        p_pixels[i + 2] = 0;
+    }
+
+}
+
+void render_police(uint32_t index, uint32_t t, uint8_t * p_pixels, uint32_t num_bytes)
+{
+    uint32_t middle = (num_bytes / 2)+3;
+    for (int i = 0; i < num_bytes; i += 3) {
+        if((i < middle && index%2==0) || (i > middle && index%2==1)){
+            p_pixels[i]     = 0;
+            p_pixels[i + 1] = t%20 < 10 ? 100 : 0;
+            p_pixels[i + 2] = 0;
+        }else{
+            p_pixels[i]     = 0;
+            p_pixels[i + 1] = 0;
+            p_pixels[i + 2] =  t%20 > 10 ? 100 : 0;;
+        }
+
+    }
+}
+
+
 static effect_render_cb effects[] = {
-    render_colors,
-    render_constant,
+  //  render_colors,
+  //  render_constant,
+   // render_car,
+   render_police,
 };
 
 static int effect_count = sizeof(effects)/sizeof(effects[0]);
@@ -61,8 +109,8 @@ void effect_next(void)
 
 void effect_show(uint32_t t)
 {
-    effects[current_effect](t, g_pixels[0], sizeof(g_pixels[0]));
-    effects[current_effect](t, g_pixels[1], sizeof(g_pixels[1]));
+    effects[current_effect](0, t, g_pixels[0], sizeof(g_pixels[0]));
+    effects[current_effect](1, t, g_pixels[1], sizeof(g_pixels[1]));
 
     ws2812b_write(g_pixels[0], sizeof(g_pixels[0]), CONN_11_GPIO_Port, CONN_11_Pin);
     ws2812b_write(g_pixels[1], sizeof(g_pixels[1]), CONN_12_GPIO_Port, CONN_12_Pin);
